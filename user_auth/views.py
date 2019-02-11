abd50f03a3231f9e3bb4dd56bae08e714a50fe62
webApp <webapp@webApp-no-Air.mshome.net>
2019-02-11 21:38:20 +0900

    wip

--- user_auth/views.py
+++ user_auth/views.py
@@ -13,3 +13,7 @@ def top_page(request):
 def talk_page(request):
     user_auth = UserSocialAuth.objects.get(user_id=request.user.id)
     return render(request, 'talk-page.html', {'user_name': user_auth.user.username})
+
+def room(request):# TODO:あとで外す
+    room_key = "r4)bUzzaqUz&s2Ys1sAlmY"
+    return render(request, 'room.html', {'room_key': room_key})
