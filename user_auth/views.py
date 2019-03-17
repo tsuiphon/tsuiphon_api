c0756bcfe5774a6cdfa364add73958a1bcdf17a1
webApp <webapp@webApp-no-Air.mshome.net>
2019-03-17 22:38:23 +0900

    ajax化

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from .session_helpers import *
+import json
 
 @login_required
 def top_page(request):
@@ -18,7 +20,7 @@ def talk_page(request):
         'avatar_url': request.session['avatar_url'],
     }
     return render(request, 'talk-page.html', render_params)
-def room(request):# TODO: API化する
+def room(request):
     before_action(request)
     if "talk_id" in request.GET:
         talk_id = request.GET.get("talk_id")
@@ -33,4 +35,9 @@ def room(request):# TODO: API化する
     else:
         pair_user_profile = get_user_profile(request.session['access_token'],request.session['access_secret'],room.user2_uid)
     pair_avatar_url = pair_user_profile['profile_image_url'].replace("_normal","")#大きいサイズに変換するために_normalを消す
-    return render(request, 'room.html', {'room_password': room.password, 'pair_avatar_url': pair_avatar_url})
+    pair_user_name = pair_user_profile['name']
+    pair_screen_name = pair_user_profile['screen_name']
+    data = {'room_password': room.password, 'pair_avatar_url': pair_avatar_url, 'pair_user_name': pair_user_name, 'pair_screen_name': pair_screen_name}
+    json_str = json.dumps(data, ensure_ascii=False)
+    response = HttpResponse(json_str, content_type='application/json; charset=UTF-8')
+    return response
